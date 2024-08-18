3232aa740945c00cefb9997ec2d4a70b42b04df9
superherozhw <150499907+superherozhw@users.noreply.github.com>
2024-08-18 23:20:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oUYL66PYOMcEEVjVY-DDXVn0-rr0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ZyJmLiE7neL31rqpNefdVfDPljHbWmgRYu0NaWtLnko',
+      useTemplateId: 'mF8oMYtUFLuRm17AkYsvZff7B5nwd7I18PzmF4A9S2E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
