b8aec6e5d675752d50f9ad58dc4cb3a3f3143590
superherozhw <150499907+superherozhw@users.noreply.github.com>
2024-08-20 20:37:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,8 @@
  */
 const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
-  USE_PASSAGE: 'push-plus',
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'server-chan',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
@@ -23,7 +24,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '汤圆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'df8e6e103c19400fa3b9810942eb97ea',
+      id: 'SCT253258T9183VZkRPksYB3jdcCIjblpl',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
